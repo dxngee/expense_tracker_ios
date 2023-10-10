d7019f33463a59236b988d27f80b08995cb9f7ef
dxngee <dxngee@gmail.com>
2023-10-10 03:00:25 +0300

    Initial Commit

---
+++ expense_tracker/expense_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expense_trackerApp.swift
+//  expense_tracker
+//
+//  Created by Danil Denisov on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct expense_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
